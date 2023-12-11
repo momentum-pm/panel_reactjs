2efc040fd092c4e189730601fe070982760ef88c
reza <rmoslemi1994@gmail.com>
2023-12-11 13:35:31 +0330

    fixed

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 git pull origin main
 npm run build
 rm -rf /var/www/panel_reactjs/build/
-cp -r /srv/momentum-pm/panel_reactjs/-reactjs/build/ /var/www/panel_reactjs/
+cp -r /srv/momentum-pm/panel_reactjs/build/ /var/www/panel_reactjs/
 chmod -R 777 /var/www/panel_reactjs/build/
 systemctl restart nginx
